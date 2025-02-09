1ef64535ecf7f7d450f20279379b8c03848cfb63
radykov <N/A>
2025-02-09 16:36:17 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
+// Import Workbox libraries from CDN
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 // Pre-cache the static assets (if any, like index.html, CSS, JS, etc.)
-precacheAndRoute(self.__WB_MANIFEST);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 // Cache all GET requests that match the current origin
-registerRoute(
+workbox.routing.registerRoute(
     ({ request, url }) => {
         // Match all GET requests
         return request.method === 'GET' && url.origin === self.location.origin;
     },
-    new StaleWhileRevalidate({
+    new workbox.strategies.StaleWhileRevalidate({
         cacheName: 'dynamic-cache', // Name of the cache
     })
 );
