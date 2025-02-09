6cfee81c4567ee3a3e6714d903e99debcbf90f79
radykov <N/A>
2025-02-09 21:10:35 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
     '/index.html',
-    '/static/css/main.css',
-    '/static/js/main.js',
+    '/static/',
+    '/images/',
+    '/stories',
 ];
 
 self.addEventListener('install', (event) => {
@@ -14,21 +15,21 @@ self.addEventListener('install', (event) => {
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('/stories/')) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then((cache) => {
-                return cache.match(event.request).then((cachedResponse) => {
-                    const fetchPromise = fetch(event.request).then((networkResponse) => {
-                        // Update the cache with the latest version of the file
-                        cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    });
+// self.addEventListener('fetch', (event) => {
+//     if (event.request.url.includes('/stories/')) {
+//         event.respondWith(
+//             caches.open(CACHE_NAME).then((cache) => {
+//                 return cache.match(event.request).then((cachedResponse) => {
+//                     const fetchPromise = fetch(event.request).then((networkResponse) => {
+//                         // Update the cache with the latest version of the file
+//                         cache.put(event.request, networkResponse.clone());
+//                         return networkResponse;
+//                     });
 
-                    // If the cache is still valid, return the cached version, otherwise fetch a new one
-                    return cachedResponse || fetchPromise;
-                });
-            })
-        );
-    }
-});
+//                     // If the cache is still valid, return the cached version, otherwise fetch a new one
+//                     return cachedResponse || fetchPromise;
+//                 });
+//             })
+//         );
+//     }
+// });
